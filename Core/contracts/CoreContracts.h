d9f9cad0a6f21e9241064059f3ec3c981940e289
RebornedTigrex <157234124+RebornedTigrex@users.noreply.github.com>
2026-03-04 21:32:44 +0300

    В Core добавлены новые контракты. Будущая замена interfaces

---
+++ Core/contracts/CoreContracts.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "contracts/Primitives.h"
+#include "contracts/IMessage.h"
+#include "contracts/IEventBus.h"
+#include "contracts/IModule.h"
+#include "contracts/IAction.h"
+#include "contracts/IAgent.h"
+#include "contracts/IFeatureManager.h"
+#include "contracts/IModuleRegistry.h"
